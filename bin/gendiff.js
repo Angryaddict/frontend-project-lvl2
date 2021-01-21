0f7fc118dcc72683895d899fe60911411fd5313c
Evgeniy Evstafiev <angryaddict@gmail.com>
2021-01-21 15:11:09 +0300

    #2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,9 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
+    .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
-
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 program.parse();
