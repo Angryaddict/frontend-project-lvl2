6effbf261a5f055e048dd556b8f6c18be43e23a9
Evgeniy Evstafiev <angryaddict@gmail.com>
2021-01-29 16:19:47 +0300

    fix mutations

--- src/diff.js
+++ src/diff.js
@@ -6,35 +6,25 @@ export default function diff(filepath1, filepath2) {
   const o2 = getData(filepath2);
   function compare(obj1, obj2) {
     const merged = { ...obj1, ...obj2 };
-    const objKeys = Object.keys(merged).sort();
+    const objKeys = Object.keys(merged).concat().sort();
     const result = objKeys.reduce((acc, key) => {
-      const node = {};
-      node.name = key;
       if (key in obj1 && key in obj2) {
         if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object') {
-          node.children = compare(obj1[key], obj2[key]);
-          acc.push(node);
-          return acc;
+          return [...acc, { name: key, children: compare(obj1[key], obj2[key]) }];
         }
         if (obj1[key] === obj2[key]) {
-          node.status = 'unchanged';
-          node.value = obj1[key];
+          return [...acc, { name: key, status: 'unchanged', value: obj1[key] }];
         }
         if (obj1[key] !== obj2[key]) {
-          node.status = 'changed';
-          node.value = obj2[key];
-          node.oldValue = obj1[key];
+          return [...acc, { name: key, status: 'changed', value: obj2[key], oldValue: obj1[key] }];
         }
       }
       if (key in obj1 && !(key in obj2)) {
-        node.status = 'deleted';
-        node.value = obj1[key];
+        return [...acc, { name: key, status: 'deleted', value: obj1[key] }];
       }
       if (!(key in obj1) && key in obj2) {
-        node.status = 'added';
-        node.value = obj2[key];
+        return [...acc, { name: key, status: 'added', value: obj2[key] }];
       }
-      acc.push(node);
       return acc;
     }, []);
     return result;
